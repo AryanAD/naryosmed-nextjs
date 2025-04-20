3287f2e6fda76e5bcb32c5a910ec984fb2e5284d
AryanAD <aryanhohai@gmail.com>
2025-04-20 23:20:55 +0545

    added leaflet map

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../../components/Map"), {
+        loading: () => <div>Loading Map...</div>,
+        ssr: false,
+      }),
+    []
+  );
+
+  return (
+    <div>
+      ContactUs
+      <div className="h-[330px] w-[100dvw]">
+        <Map posix={[4.6945, -74.141]} />
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
